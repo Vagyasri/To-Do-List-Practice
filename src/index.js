fa11594819412a82fcf19e36130f96b0b2f163d4
Lucky <vagyasri2011@gmail.com>
2021-09-23 08:56:37 +0200

    Create an array to contain the list

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import _ from 'lodash';
 import './style.css';
 
-// function component() {
-//     const element = document.createElement('div');
-  
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-//     return element;
+const container = document.querySelector('[data]')
+
+const todoList = [];
+
+
+
+// const myArr = [
+//   {
+//     description: "Wash the Dishes",
+//     completed: True,
+//     index: 1
+//   },
+//   {
+//     description: "Complete to-do list Project",
+//     completed: False,
+//     index: 2
 //   }
+// ];
+
+// const todo = list() => {
+//   const ul = document.createElement('ul');
+//   const li = document.createElement('li');
+
+//   li.innerHtml =  ({description, completed, index}) = myArr;
   
-//   document.body.appendChild(component());
-
-const myArr = [
-  {
-    description: "Wash the Dishes",
-    completed: True,
-    index: 1
-  },
-  {
-    description: "Complete to-do list Project",
-    completed: False,
-    index: 2
-  }
-];
-
-const todo = list() => {
-  const ul = document.createElement('ul');
-  const li = document.createElement('li');
-
-  li.innerHtml =  ({description, completed, index}) = myArr;
-  
-};
+// };
